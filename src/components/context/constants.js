1a1c8a1ff9693226801fb71383fca1acf04f1516
gauravagrahari <gauravagrahari158@gmail.com>
2024-05-05 16:27:15 +0530

    Revert "push"
    This reverts commit bbdf52824bf6594a1bbbfc051f8617289a82ef40.

--- src/components/context/constants.js
+++ src/components/context/constants.js
@@ -1,12 +1,9 @@
 // config.js
 // import { URL } from '@env';
 
-// const config = {
-//   URL: 'http://Prod-env.eba-tabt2pxm.ap-south-1.elasticbeanstalk.com', // Provide a default value if URL is not defined
-//   // URL: 'http://uat-env.eba-epp52dpv.ap-south-1.elasticbeanstalk.com', // Provide a default value if URL is not defined
-// };
 const config = {
-  URL: '/api',  // Proxying all requests through Netlify with '/api' prefix
+  URL: 'http://Prod-env.eba-tabt2pxm.ap-south-1.elasticbeanstalk.com', // Provide a default value if URL is not defined
+  // URL: 'http://uat-env.eba-epp52dpv.ap-south-1.elasticbeanstalk.com', // Provide a default value if URL is not defined
 };
 
 export default config;
