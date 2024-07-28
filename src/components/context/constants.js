28d774860af08ef591e49cb19e557bedd7fd145c
gauravagrahari <gauravagrahari158@gmail.com>
2024-07-28 23:25:13 +0530

    push

--- src/components/context/constants.js
+++ src/components/context/constants.js
@@ -2,8 +2,7 @@
 // import { URL } from '@env';
 
 const config = {
-  URL: 'http://Prod-env.eba-tabt2pxm.ap-south-1.elasticbeanstalk.com', // Provide a default value if URL is not defined
-  // URL: 'http://uat-env.eba-epp52dpv.ap-south-1.elasticbeanstalk.com', // Provide a default value if URL is not defined
+  URL: '', // Provide a default value if URL is not defined
 };
 
 export default config;
